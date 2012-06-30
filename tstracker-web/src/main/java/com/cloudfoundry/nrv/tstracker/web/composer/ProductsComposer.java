69dd436b74f49757f2cd393d9d6ec0957ac67e13
Antonino Salas <asalas@winter26-pc.(none)>
2012-06-30 13:32:35 -0500

    Correción del ejemplo de autenticación y autorización con Apache Shiro

--- tstracker-web/src/main/java/com/cloudfoundry/nrv/tstracker/web/composer/ProductsComposer.java
+++ tstracker-web/src/main/java/com/cloudfoundry/nrv/tstracker/web/composer/ProductsComposer.java
@@ -1,7 +1,16 @@
 package com.cloudfoundry.nrv.tstracker.web.composer;
 
+import org.apache.shiro.SecurityUtils;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 
 public class ProductsComposer extends GenericForwardComposer {
 
+	public void onClick$home() {
+		execution.sendRedirect("/home.zul");
+	}
+
+	public void onClick$logout() {
+		SecurityUtils.getSubject().logout();
+		execution.sendRedirect("/home.zul");
+	}
 }
